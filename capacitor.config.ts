830dc47ae6c8ed6fcdb04c32671d373faaf6719b
202355W <202355W@mymail.nyp.edu.sg>
2022-11-29 15:43:38 +0800

    Bootstrap project

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'sg.edu.nyp.sit.jobeasy',
+  appName: 'JobEasy',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
---
+++ src/router/index.ts
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from '@ionic/vue-router'
+import { RouteRecordRaw } from 'vue-router'
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    redirect: '/home',
+  },
+  {
+    path: '/home',
+    component: Home,
+    // meta: { transition: 'slide-left' },
+  },
+  {
+    path: '/about',
+    component: About,
+    // meta: { transition: 'slide-right' },
+  },
+]
+
+// https://vitejs.dev/guide/env-and-mode.html
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+})
+
+export default router
